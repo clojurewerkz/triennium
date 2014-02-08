b3637f7361ce4ac0c89ec2ae7a544129d7395e00
Michael Klishin <mklishin@gopivotal.com>
2014-02-08 21:26:51 +0400

    Description

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clojurewerkz/triennium "1.0.0-SNAPSHOT"
-  :description "FIXME"
+  :description "Small library that strives to provide efficient
+topic routing implementations for RabbitMQ and MQTT topic schemes"
   :dependencies [[org.clojure/clojure        "1.5.1"]
                  [org.clojure/core.incubator "0.1.3"]]
   :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
