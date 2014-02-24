43dd3990fe86004901fbb52862a01ac6200abb2f
Michael Klishin <michael@clojurewerkz.org>
2014-02-24 23:23:33 +0400

    1.0.0-beta2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojurewerkz/triennium "1.0.0-beta2-SNAPSHOT"
+(defproject clojurewerkz/triennium "1.0.0-beta2"
   :description "Small library that strives to provide efficient
 topic routing implementations for RabbitMQ and MQTT topic schemes"
   :dependencies [[org.clojure/clojure        "1.5.1"]
