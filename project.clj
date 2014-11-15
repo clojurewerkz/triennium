203e6630a42fc37fb682c1e40776cedcc5a2b653
Michael Klishin <michael@clojurewerkz.org>
2014-11-15 19:37:47 +0300

    Test against 1.5, drop 1.4 profile

--- project.clj
+++ project.clj
@@ -1,10 +1,9 @@
 (defproject clojurewerkz/triennium "1.0.0-beta3-SNAPSHOT"
   :description "Small library that strives to provide efficient
                 topic routing implementations for MQTT topic schemes"
-  :dependencies [[org.clojure/clojure        "1.5.1"]
+  :dependencies [[org.clojure/clojure        "1.6.0"]
                  [org.clojure/core.incubator "0.1.3"]]
-  :profiles {:1.4 {:dependencies [[org.clojure/clojure "1.4.0"]]}
-             :1.6 {:dependencies [[org.clojure/clojure "1.6.0"]]}
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.7 {:dependencies [[org.clojure/clojure "1.7.0-alpha4"]]}
              :master {:dependencies [[org.clojure/clojure "1.7.0-master-SNAPSHOT"]]}
              :dev {:resource-paths ["test/resources"]
@@ -12,7 +11,7 @@
                    :codox {:sources ["src/clojure"]
                            :output-dir "doc/api"}
                    :dependencies [[reiddraper/simple-check "0.5.6"]]}}
-  :aliases {"all" ["with-profile" "dev:dev,1.4:dev,1.6:dev,1.7:dev,master"]}
+  :aliases {"all" ["with-profile" "dev:dev,1.5:dev,1.7:dev,master"]}
   :repositories {"sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail}}
